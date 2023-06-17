5f52861e2b6f21be45e88f88afc764deb1d0f020
-chikkyb <chikkybaby2020@gmail.com>
2023-06-17 08:23:46 -0700

    5th printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-	/*
-	 * This program prints a message
-	 * using the printf function
-	 */
-	printf("With proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
