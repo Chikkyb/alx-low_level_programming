af4cb76c2192a615b2259cff2aeadcecc3f750cd
-chikkyb <chikkybaby2020@gmail.com>
2023-06-20 13:41:52 -0700

    1st alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main-starts here
+ * Description- print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
